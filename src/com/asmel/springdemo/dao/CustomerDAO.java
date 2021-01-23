0dfce5b52d3ea359668e8fb90bd130ed4a08abd3
Aszmel <mellipone@gmail.com>
2021-01-23 17:10:01 +0100

    finished project

---
+++ src/com/asmel/springdemo/dao/CustomerDAO.java
@@ -0,0 +1,16 @@
+package com.asmel.springdemo.dao;
+
+import java.util.List;
+import com.asmel.springdemo.entity.Customer;
+
+public interface CustomerDAO {
+	
+	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
+
+	public Customer getCustomer(int theId);
+
+	public void deleteCustomer(int theId);
+
+}
